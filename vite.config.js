0a31b3ac2fb07a188aeaac7b7c53ee6d3e11171e
KARTIKNAIK18 <kartiknaik560@gmail.com>
2025-09-03 22:18:54 +0530

    config file updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',       // Production build folder
+    assetsDir: 'assets',  // Folder for JS/CSS/images
+  },
+  base: './',             // Ensures assets load correctly in container/Ingress
+  server: {
+    host: '0.0.0.0',      // Accessible from any IP (required for container/K8s)
+    port: 5173,            // Dev server port
+  },
 })
